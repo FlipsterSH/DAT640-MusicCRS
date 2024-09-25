6d0037735bba7d1a2239e22d864371154627c153
FlipsterSH <filippo1339@gmail.com>
2024-09-25 13:22:28 +0200

    yolo

--- main.py
+++ main.py
@@ -4,3 +4,25 @@
 def get_commands():
     return ["/add","/delete", "/clear", "/list"]
 
+def add(command, playlist):
+    action = command.split(" ")[0]
+    song = command.split(" ")[1:]
+
+    if action == "/add":
+    print(action)
+    print(song)
+
+
+    playlist.append(song)
+    return playlist
+
+
+if __name__ == "__main__":
+    list = add("/add shine bright", [])
+    print(list)
+
+
+
+
+
+
