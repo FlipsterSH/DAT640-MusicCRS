2d232f2b7480717c3b487159cfb7dbf60b0fa337
FlipsterSH <filippo1339@gmail.com>
2024-09-25 13:43:22 +0200

    yolo

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import streamlit as st
 from main import *
 
-commands = get_commands()
 playlist = []
 user_msgs = []
 bot_msgs = []
 
-
 messages = st.container(height=300)
 if prompt := st.chat_input("Say something"):
+    # Processing prompt
+    prompt_components = prompt.split(" ")
+    command = prompt_components[0]
+    song = " ".join(prompt_components[1:])
+
+    # Executing command
+
+    # Saving userprompt
+    user_msgs.append(prompt)
+
+
+
+    # Shows all messages in messagelist
     for i in range(len(user_msgs)):
         messages.chat_message("user").write(user_msgs[i])
         messages.chat_message("assistant").write(bot_msgs[i])
+
+    
--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 #Test file
+
+l = "/add Shine Bright"
+ll = l.split(" ")
+command = ll[0]
+song = " ".join(ll[1:])
+
+print(command)
+print(song)
